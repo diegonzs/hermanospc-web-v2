7a6af09f30095978dca838cbb783326e451bf6ef
Jose Angel Simancas Lopez <jsimancas@shokworks.io>
2021-03-09 10:38:38 -0500

    grid profile

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -1,10 +1,11 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { Layout } from 'components/layout';
 import { MainBtn } from 'components/common/main-btn';
 import RightPanelProfile from './right_panel';
 import { Avatardetails } from 'components/avatar-details';
 
 const Profile = () => {
+	const [TabAction, setTabAction] = useState('post');
 	return (
 		<Layout rightPanel={<RightPanelProfile />}>
 			<div className="header-menu">
@@ -20,6 +21,41 @@ const Profile = () => {
 					<button className="sign-out">Sign out</button>
 					<MainBtn text="Settings" type="default" onClick={() => null} />
 				</div>
+			</div>
+			<div className="item-row-info">
+				<div className="info-items">
+					<button className={TabAction == 'post' && 'active'} onClick={() => setTabAction('post')}>
+						Posts
+					</button>
+					<button className={TabAction == 'specs' && 'active'} onClick={() => setTabAction('specs')}>
+						Specs
+					</button>
+				</div>
+				{TabAction == 'post' && (
+					<div className="item-row-container">
+						<div className="item-card" />
+						<div className="item-card" />
+						<div className="item-card" />
+						<div className="item-card" />
+						<div className="item-card" />
+						<div className="item-card" />
+						<div className="item-card" />
+						<div className="item-card" />
+						<div className="item-card" />
+						<div className="item-card" />
+						<div className="item-card" />
+						<div className="item-card" />
+					</div>
+				)}
+				{TabAction == 'specs' && (
+					<div className="item-row-container">
+						<div className="item-card" />
+						<div className="item-card" />
+						<div className="item-card" />
+						<div className="item-card" />
+						<div className="item-card" />
+					</div>
+				)}
 			</div>
 			<style jsx>{`
 				@import 'variables.scss';
@@ -40,6 +76,36 @@ const Profile = () => {
 					font-size: 16px;
 					margin-right: 3rem;
 					cursor: pointer;
+				}
+				.item-row-container {
+					display: grid;
+					grid-template-columns: repeat(3, 1fr);
+				}
+				.item-row-container .item-card {
+					background: url(images/profile-page/pic-user-post.jpg) no-repeat;
+					height: 177px;
+					border-radius: 24px;
+					max-width: 278px;
+					margin-bottom: 1rem;
+				}
+				.item-row-info .info-items button {
+					letter-spacing: 0px;
+					color: #bfbfbf;
+					background: transparent;
+					border: 0;
+					outline: none;
+					font-size: 16px;
+					margin-right: 2rem;
+					cursor: pointer;
+				}
+				.item-row-info {
+					margin-top: 2rem;
+				}
+				.info-items {
+					margin-bottom: 1rem;
+				}
+				.active {
+					color: #efefef !important;
 				}
 			`}</style>
 		</Layout>
