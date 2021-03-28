ee3d38f678c8fbc59e01f89f95a5697cfabc4759
Jose Angel Simancas Lopez <jsimancas@shokworks.io>
2021-03-28 12:45:08 -0500

    title and buttons

--- pages/profile-settings/index.jsx
+++ pages/profile-settings/index.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
 import { Layout } from 'components/layout';
+import { MainBtn } from 'components/common/main-btn';
 
 const ProfileSettings = () => {
 	return (
 		<Layout>
-			<div> hola mundo </div>
+			<div className="header-menu">
+				<h1 className="title">Settings</h1>
+				<div className="menu-buttons">
+					<button className="cancel-button">Cancel</button>
+					<MainBtn text="Save" type="default" />
+				</div>
+			</div>
+			<style jsx>{`
+				@import 'variables.scss';
+				.title {
+					letter-spacing: 0px;
+					color: #efefef;
+					font-size: 40px;
+				}
+				.header-menu {
+					display: flex;
+					justify-content: space-between;
+					align-items: center;
+				}
+				.menu-buttons {
+					display: flex;
+				}
+				.cancel-button {
+					letter-spacing: 0px;
+					color: #efefef;
+					background: transparent;
+					border: 0;
+					outline: none;
+					font-size: 16px;
+					margin-right: 3rem;
+					cursor: pointer;
+				}
+			`}</style>
 		</Layout>
 	);
 };
