02d0e4f66ee9681e3c84b668d0eb99ec001595a6
Jose Angel Simancas Lopez <jsimancas@shokworks.io>
2021-03-30 17:21:37 -0500

    some change to upload image imput

--- components/common/upload-image-input/upload-image-input.jsx
+++ components/common/upload-image-input/upload-image-input.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 
-export const UploadImageInput = () => {
+export const UploadImageInput = ({ previous_img }) => {
 	const [img_preview, set_img_preview] = useState(null);
 	const handleChange = (e) => {
 		set_img_preview(URL.createObjectURL(e.target.files[0]));
 	};
 	return (
 		<div className="upload-container">
-			{img_preview && <div className="image-preview" />}
+			{img_preview || previous_img ? <div className="image-preview" /> : null}
 			<label>
 				{img_preview ? 'Change' : <img width="60" src="/images/icons/plus.svg" alt="add image" />}
 				<input className="custom-file-input" type="file" onChange={(e) => handleChange(e)} />
@@ -20,7 +20,7 @@ export const UploadImageInput = () => {
 					max-width: 163px;
 					width: 100%;
 					height: 101px;
-					background: url('${img_preview}') center center;
+					background: url('${img_preview ? img_preview : previous_img}') center center;
 					background-repeat: no-repeat;
 					background-size: cover;
 					border-radius: 16px;
